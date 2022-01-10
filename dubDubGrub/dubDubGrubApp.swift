2aa8e4fabc1f9317e9c5d4252ca71b160ded237b
Lce <luciancerbu87@gmail.com>
2022-01-11 00:04:21 +0200

    Initial Setup

---
+++ dubDubGrub/dubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  dubDubGrubApp.swift
+//  dubDubGrub
+//
+//  Created by Lucian Cerbu on 11.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct dubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
