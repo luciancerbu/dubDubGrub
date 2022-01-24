efd8a2b549c2ff231caf32894aba8c7bee2602dc
Lucian Cerbu <luciancerbu87@gmail.com>
2022-01-24 17:05:43 +0200

    [4.4]Showing User Location on the Map
    Work with CLLocationManager to show the user's location on the map.
    
    CLLocationManager Documentation - https://developer.apple.com/documentation/corelocation/cllocationmanager

--- dubDubGrub/Utilities/AlertItem.swift
+++ dubDubGrub/Utilities/AlertItem.swift
@@ -19,4 +19,16 @@ struct AlertContext {
     static let unableToGetLocation = AlertItem(id: nil, title: Text("Location Error"),
                                            message: Text("Unable to retrieve location at this time.\n Please try again."),
                                            dismissButton: .default(Text("Ok")))
+    
+    static let locationRestricted = AlertItem(id: nil, title: Text("Location Restricted"),
+                                           message: Text("You location is restricted. This may be due to parental controls."),
+                                           dismissButton: .default(Text("Ok")))
+    
+    static let locationDenied = AlertItem(id: nil, title: Text("Location Denied"),
+                                           message: Text("Dub Dub Grub does not have permission to access your location. To change that go to your phone settings > Dub Dub Grub > Location"),
+                                           dismissButton: .default(Text("Ok")))
+    
+    static let locationDisabled = AlertItem(id: nil, title: Text("Location Service Disabled"),
+                                           message: Text("Your phone's location services are disabled. To change go to your phone settings > Privacy > Location Services"),
+                                           dismissButton: .default(Text("Ok")))
 }
